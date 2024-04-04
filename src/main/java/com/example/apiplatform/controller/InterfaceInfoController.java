f87e88518352c91e4e1f422cfa1c90a4ac3e9ce6
JaysCode <jayscode@user.noreply.gitee.com>
2024-04-04 15:13:04 +0800

    增加了一些工具类和给interface各种请求建立了专门dto类，便利前端

--- src/main/java/com/example/apiplatform/controller/InterfaceInfoController.java
+++ src/main/java/com/example/apiplatform/controller/InterfaceInfoController.java
@@ -1,9 +1,25 @@
 package com.example.apiplatform.controller;
 
+import com.example.apiplatform.service.InterfaceInfoService;
+import com.example.apiplatform.service.UserService;
+import com.jays.client.JaysApiClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/interfaceInfo")
 public class InterfaceInfoController {
+    @Resource
+    private InterfaceInfoService interfaceInfoService;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private JaysApiClient jaysApiClient;
+
+    //增删改查
+
 }
