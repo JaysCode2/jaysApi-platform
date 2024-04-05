2098ac5aa59ab25acf7666b53d7146d3e78da40a
JaysCode <jayscode@user.noreply.gitee.com>
2024-04-06 00:50:23 +0800

    增加了gateway子项目

---
+++ jaysApi-gateway/src/main/java/com/jays/jaysapigateway/JaysApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jays.jaysapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaysApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaysApiGatewayApplication.class, args);
+    }
+
+}
