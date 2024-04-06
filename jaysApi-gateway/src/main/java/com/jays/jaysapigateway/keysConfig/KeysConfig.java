16e281297c4f8c096958dd66ba427d5c1ce364d6
JaysCode <jayscode@user.noreply.gitee.com>
2024-04-06 20:46:48 +0800

    为@CongigurationProperties注解的使用导入依赖

--- jaysApi-gateway/src/main/java/com/jays/jaysapigateway/keysConfig/KeysConfig.java
+++ jaysApi-gateway/src/main/java/com/jays/jaysapigateway/keysConfig/KeysConfig.java
@@ -3,16 +3,20 @@ package com.jays.jaysapigateway.keysConfig;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 @Data
+@Configuration
 @Component
 //不知道为什么这个注解注入不进去
-//@ConfigurationProperties(prefix = "keys")
+//用这个注解要导入依赖
+@ConfigurationProperties("keys.client")
 public class KeysConfig {
 //    @Value("${keys.accessKey}")
 //    private String accessKey;
 
-    @Value("${keys.secretKey}")
+//    @Value("${keys.secretKey}")
     private String secretKey;
 }
 
